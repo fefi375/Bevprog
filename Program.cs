df2c99c1d043651a1f96e981a36a83e3074dd776
fefi375 <113167069+fefi375@users.noreply.github.com>
2022-12-07 18:36:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+class Program
+{   static public void Main(String[] args)
+    {
+        StreamWriter sw = new StreamWriter("kozos.txt");
+        sw.WriteLine("Ez a közös fájl (C#)");
+        sw.Close();
+    }
+}
