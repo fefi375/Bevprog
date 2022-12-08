17a24b0537f33fa0b9f28012f0089bff1fa51803
fefi375 <113167069+fefi375@users.noreply.github.com>
2022-12-08 19:56:55 +0200

    Add files via upload

---
+++ feladat1.py
@@ -0,0 +1,27 @@
+def main():
+    text="""
+    Cbcq Dgyk!
+
+    Dmeybh kce cew yrwyg hmrylyaqmr:
+    rylsjb kce y Nwrfml npmepykmxyqg lwcjtcr!
+
+    Aqmimjjyi:
+
+    Ynyb"""
+    newtext=""""""
+    for i in range(0, len(text)): #10 #: = 58
+        if ord(text[i])+2 > 90 and ord(text[i])<97:
+            newtext=newtext+chr(ord(text[i])-24)
+        elif ord(text[i])>63 and ord(text[i])+2<=122:
+           newtext=newtext+chr(ord(text[i])+2)
+        elif ord(text[i])+2>122:
+            newtext=newtext+chr(ord(text[i])-24)
+        else:
+            newtext=newtext+text[i]
+            
+        
+    print(newtext)
+        
+    
+if __name__=="__main__":
+    main()
